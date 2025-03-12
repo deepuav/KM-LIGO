f4407e7a151e728a8df82dcd027805146477fffa
Daehan Lee <daehanlee@postech.ac.kr>
2025-03-12 15:41:53 +0900

    Remove unnecessary computation in RemovePointsFarFromLocation()

--- cpp/genz_icp/core/VoxelHashMap.cpp
+++ cpp/genz_icp/core/VoxelHashMap.cpp
@@ -228,9 +228,9 @@ void VoxelHashMap::AddPoints(const std::vector<Eigen::Vector3d> &points) {
 }
 
 void VoxelHashMap::RemovePointsFarFromLocation(const Eigen::Vector3d &origin) {
+    const auto max_distance2 = map_cleanup_radius_ * map_cleanup_radius_;
     for (const auto &[voxel, voxel_block] : map_) {
         const auto &pt = voxel_block.points.front();
-        const auto max_distance2 = map_cleanup_radius_ * map_cleanup_radius_;
         if ((pt - origin).squaredNorm() > (max_distance2)) {
             map_.erase(voxel);
         }
